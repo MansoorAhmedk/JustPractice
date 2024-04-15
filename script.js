92fa550825ce504ea6e3dc2a440ef1b736f97400
MansoorAhmedk <abbasimansoor97@gmail.com>
2024-04-15 15:36:50 +0500

    practice files added

---
+++ script.js
@@ -0,0 +1,209 @@
+// let a = 2;
+// let b = String(a);
+// console.log("Data Type of a is "+ String(a));
+// console.log("Data Type of a is "+ b);
+
+
+
+let a = 7;
+let b = 5;
+
+let c = 3;
+let d = 3;
+
+// let sum = a+b;
+// let sub = a - b
+// let mult = a*b;
+// let divide = a/b;
+// let mod = a % b
+
+// let exp = c**b
+
+// let inc = ++a
+// let dec = --a
+
+// console.log(dec);
+// console.log(inc);
+
+
+// a **= 10
+// console.log(a);
+
+// console.log(a >= b);
+
+// console.log(a!=a);
+
+// if (a == 7) {
+//     console.log("You are a good student.");
+// }
+// else{
+//     console.log("You are not a good student");
+// }
+
+
+// let score = prompt("Enter your score: ");
+// if(score > 100){
+//     console.log("Score should be 0 to 100"); 
+// }
+// else if (score >= 90 ){
+//     console.log(" you secred A grade.");
+   
+// }
+// else if (score >= 80 ) {
+//     console.log(" you secred B grade.");
+// }else if (score >= 70) {
+//     console.log(" you secred C grade.");
+// }else if (score >= 60) {
+//     console.log(" you secred D grade.");
+// }else if (score >= 50) {
+//     console.log(" you secred F grade.");
+// }
+// else{
+//     console.log(" you are considered to apply in next year.");
+// }
+
+
+
+// let password = prompt("Enter your Password: ")
+
+// if(password == "Mansoor"){
+//     console.log("Welcome Mansoor!");
+// }
+// else{
+//     console.log("Invalid Credentials.");
+// }
+
+// let password = prompt("Enter your Password!!");
+
+// switch (password) {
+//     case "Rafiq":
+//         console.log("Welcome Rafiq!");
+//         break;
+//     case "Mansoor":
+//         console.log("Welcome Mansoor!");x
+//         break;
+//     case "Shahzad":
+//         console.log("Welcome Shahzad!");
+//         break;
+
+//     default:
+//         console.log("Incorrect Password!");
+//         break;
+// }
+
+
+
+// Using a for loop to calculate the sum of numbers from 1 to 100
+// let total = 0;
+// for (let i = 1; i <= 100; i++) {
+//   total += i;
+// }
+// console.log("Sum of numbers from 1 to 100:", total);
+
+
+// // Using a while loop to print even numbers from 1 to 10
+// let num = 1;
+// while (num <= 10) {
+//   if (num % 2 === 0) {
+//     console.log(num);
+//   }
+//   num++;
+// }
+
+
+// let num1 =23;
+
+// if(num1 == 23){
+//   console.log("Welcome!");
+// document.write("Welcome")
+// }
+// else{
+//   console.log("Try Again!!");
+// }
+
+
+// Loops 
+
+// for(let i = 2; i <100; i++){
+//   console.log("Mansoor", i);
+// }
+
+let num2 =1;
+// while(num2 <= 5){
+//   console.log("Welcome", num2);
+//   num2++
+// }
+
+// do {
+//   console.log("Hi! Welcome", num2);
+// num2++
+  
+// } while (num2 < 7)
+
+// for(let a = 0; a<5; a++){
+//   for(let b = 0; b<=a; b++){
+//     console.log('* ', b);
+//   }
+// }
+
+// let rows = 10
+// for(let i =1; i<=rows; i++){
+//   let pattern = ''
+//   for(let j = 1; j<=i; j++){
+//     pattern -= '*'
+//   }
+//   console.log(pattern);
+// }
+
+
+// let n = 5; // row or column count
+// // defining an empty string
+// let string = "";
+
+// for(let i = 0; i < n; i++) { // external loop
+//   for(let j = 0; j < n; j++) { // internal loop
+//     string += "*";
+//     console.log(string);
+//   }
+//   // newline after each row
+//   string += "\n";
+  
+// }
+// // printing the string
+
+
+
+// functions
+
+function add(p,p1){
+  let c = p + p1;
+  return c;
+}
+
+
+// console.log(add(1000,900));
+
+
+
+
+// const element =document.querySelector('body');
+// element.style.backgroundColor = 'teal'
+
+
+console.log(add(100,90));
+
+
+// let guess = prompt("Enter your Operation; i.e : Addition, Subtraction, Multiplication or Division");
+
+// if (guess == a){
+//   let firstnum = prompt("Enter First number: ")
+//   let secondnum = prompt("Enter second number: ")
+
+//   console.log(add(firstnum,secondnum));
+// }else{
+
+// }
+
+
+
+
